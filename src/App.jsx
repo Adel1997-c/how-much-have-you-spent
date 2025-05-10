029de1c23732aa6348de5300c62c7bb146985023
Luca A. <aadelalmashi1@gmail.com>
2025-05-10 17:42:06 +0300

    Fix: Actually render HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
-import React from 'react';
-import HomePage from './pages/HomePage';
+import React from "react";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return <HomePage />;
